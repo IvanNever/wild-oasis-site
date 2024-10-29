5422ba3b937d37eee56e146b4d6d73255b8a6c18
Ivan Neverovsky <ivan@neverovsky.com.ua>
2024-10-29 17:00:26 +0200

    added account nested pages and layout

--- app/account/page.js
+++ app/account/page.js
@@ -3,7 +3,11 @@ export const metadata = {
 };
 
 function Page() {
-  return <h1>Account Page</h1>;
+  return (
+    <h2 className="font-semibold text-2xl text-accent-400 mb-4">
+      Welcome, Ivan
+    </h2>
+  );
 }
 
 export default Page;
