2557aa41bad06b93bea984451a8b1f35885937bd
Ivan Neverovsky <ivan@neverovsky.com.ua>
2024-10-29 15:47:41 +0200

    change main layout and pages components

--- app/layout.js
+++ app/layout.js
@@ -4,13 +4,12 @@ import Logo from "@/app/_components/Logo";
 import "@/app/_styles/globals.css";
 
 import { Josefin_Sans } from "next/font/google";
+import Header from "@/app/_components/Header";
 
 const josefin = Josefin_Sans({
   subsets: ["latin"],
   display: "swap",
 });
-
-console.log(josefin);
 
 export const metadata = {
   title: {
@@ -25,13 +24,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen`}
+        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen flex flex-col relative`}
       >
-        <header>
-          <Logo />
-          <Navigation />
-        </header>
-        <main>{children}</main>
+        <Header />
+        <div className="flex-1 px-8 py-12 ">
+          <main className="max-w-7xl mx-auto">{children}</main>
+        </div>
       </body>
     </html>
   );
