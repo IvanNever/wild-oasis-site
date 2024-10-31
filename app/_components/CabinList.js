6cb2c50c51465ff09565d980495590d0665b9f2d
Ivan Neverovsky <ivan@neverovsky.com.ua>
2024-10-31 18:31:38 +0200

    added booking reservation and change some client / server componetns relations

--- app/_components/CabinList.js
+++ app/_components/CabinList.js
@@ -3,16 +3,30 @@
 import CabinCard from "@/app/_components/CabinCard";
 import { getCabins } from "@/app/_lib/data-service";
 
-async function CabinList() {
+async function CabinList({ filter }) {
   // noStore();
 
   const cabins = await getCabins();
 
   if (!cabins.length) return null;
 
+  const filteredCabins = cabins.filter((cabin) => {
+    switch (filter) {
+      case "small":
+        return cabin.maxCapacity <= 3;
+      case "medium":
+        return cabin.maxCapacity > 3 && cabin.maxCapacity < 8;
+      case "large":
+        return cabin.maxCapacity >= 8;
+      case "all":
+      default:
+        return true;
+    }
+  });
+
   return (
     <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
-      {cabins.map((cabin) => (
+      {filteredCabins.map((cabin) => (
         <CabinCard cabin={cabin} key={cabin.id} />
       ))}
     </div>
--- app/cabins/loading.js
+++ app/cabins/loading.js
@@ -3,6 +3,7 @@ import Spinner from "@/app/_components/Spinner";
 function Loading() {
   return (
     <div className="grid items-center justify-center">
+      <p>Cabin data loading...</p>
       <Spinner />;
     </div>
   );
