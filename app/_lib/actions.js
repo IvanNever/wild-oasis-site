5807b504b0828291b14d0530de4f338b94fc4281
Ivan Neverovsky <ivan@neverovsky.com.ua>
2024-11-01 17:38:14 +0200

    deleting the guest booking

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -3,6 +3,7 @@
 import { auth, signIn, signOut } from "@/app/_lib/auth";
 import { supabase } from "@/app/_lib/supabase";
 import { revalidatePath } from "next/cache";
+import { getBookings } from "@/app/_lib/data-service";
 
 export async function updateGuest(formData) {
   const session = await auth();
@@ -17,7 +18,7 @@ export async function updateGuest(formData) {
 
   const updateData = { nationality, nationalID, countryFlag };
 
-  const { data, error } = await supabase
+  const { error } = await supabase
     .from("guests")
     .update(updateData)
     .eq("id", session.user.guestId);
@@ -27,6 +28,25 @@ export async function updateGuest(formData) {
   }
 
   revalidatePath("/account/profile");
+}
+
+export async function deleteReservation(id) {
+  const session = await auth();
+  if (!session) throw new Error("You must be logged in");
+
+  const guestBookings = await getBookings(session.user.guestId);
+
+  if (!guestBookings.map((booking) => booking.id).includes(id)) {
+    throw new Error("You are not allowed to delete this booking");
+  }
+
+  const { error } = await supabase.from("bookings").delete().eq("id", id);
+
+  if (error) {
+    throw new Error("Reservation could not be deleted");
+  }
+
+  revalidatePath("/account/reservations");
 }
 
 export async function signInAction() {
